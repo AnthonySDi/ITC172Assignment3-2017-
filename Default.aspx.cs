a7afeda0677638b0a046953ccefddb20482a7d0f
Steve Conger <steve@spconger.com>
2017-01-30 12:28:05 -0800

    Commit revised full version

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -14,12 +14,16 @@ public partial class _Default : System.Web.UI.Page
 
     protected void LoginButton_Click(object sender, EventArgs e)
     {
+        //initialize the loginClass and pass it the user and password
         LoginClass lc = new LoginClass(UserTextBox.Text, PasswordTextBox.Text);
-        int key = lc.ValidateLogin();
+        int key = lc.ValidateLogin(); //call the validation method
         if(key != 0)
         {
-            ResultLabel.Text = "Welcome";
+            //if the login is good save the key to a session variable
             Session["userKey"] = key;
+            //redirect to the other page
+            Response.Redirect("GrantRequests.aspx");
+
         }
         else
         {
